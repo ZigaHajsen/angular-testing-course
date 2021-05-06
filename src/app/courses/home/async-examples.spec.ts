5334f1ab620e994b1b62dbfbd5723de4647159ab
zigah <ziga.hajsen@agenda.si>
2021-05-06 08:49:38 +0200

    cleanup

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -30,22 +30,16 @@ describe("Async Testing Examples", () => {
 
   it("Asynchronous test example - plain Promise", fakeAsync(() => {
     let test = false;
-    console.log("Creating promise");
 
     Promise.resolve()
       .then(() => {
-        console.log("Promise first then() evaluated successfully");
-
         return Promise.resolve();
       })
       .then(() => {
-        console.log("Promise second then() evaluated successfully");
         test = true;
       });
 
     flushMicrotasks();
-
-    console.log("Running test assertions");
 
     expect(test).toBeTruthy();
   }));
@@ -79,8 +73,6 @@ describe("Async Testing Examples", () => {
   it("Asynchronous test example - Observables", fakeAsync(() => {
     let test = false;
 
-    console.log("Creating Observable");
-
     const test$ = of(test).pipe(delay(1000));
 
     test$.subscribe(() => {
@@ -88,8 +80,6 @@ describe("Async Testing Examples", () => {
     });
 
     tick(1000);
-
-    console.log("Running test assertions");
 
     expect(test).toBe(true);
   }));
