9e3d138ceb1d1953425e49e7b326b210641f33dc
zigah <ziga.hajsen@agenda.si>
2021-05-06 08:45:22 +0200

    added tests

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,7 +1,9 @@
-import { fakeAsync, flush, tick } from "@angular/core/testing";
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 describe("Async Testing Examples", () => {
-  it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
+  it("Asynchronous test example - Jasmine done()", (done: DoneFn) => {
     let test = false;
 
     setTimeout(() => {
@@ -26,7 +28,7 @@ describe("Async Testing Examples", () => {
     expect(test).toBeTruthy();
   }));
 
-  fit("Asynchronous test example - plain Promise", () => {
+  it("Asynchronous test example - plain Promise", fakeAsync(() => {
     let test = false;
     console.log("Creating promise");
 
@@ -41,8 +43,54 @@ describe("Async Testing Examples", () => {
         test = true;
       });
 
+    flushMicrotasks();
+
     console.log("Running test assertions");
 
     expect(test).toBeTruthy();
-  });
+  }));
+
+  it("Asynchronous test example - Promises + setTimeout()", fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+
+    flushMicrotasks();
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(11);
+  }));
+
+  it("Asynchronous test example - Observables", fakeAsync(() => {
+    let test = false;
+
+    console.log("Creating Observable");
+
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+      test = true;
+    });
+
+    tick(1000);
+
+    console.log("Running test assertions");
+
+    expect(test).toBe(true);
+  }));
 });
